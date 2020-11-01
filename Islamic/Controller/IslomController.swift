c6279b623a2731df1adbb9b261da97dfd45e24ae
JokeCoder <nshaxzod98@gmail.com>
2020-11-01 15:52:53 +0500

    zxzx

---
+++ Islamic/Controller/IslomController.swift
@@ -0,0 +1,24 @@
+//
+//  IslomController.swift
+//  Islamic
+//
+//  Created by coder on 29/10/20.
+//
+
+import UIKit
+
+class IslomController: UIViewController {
+
+    @IBAction func back(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+
+
+}
