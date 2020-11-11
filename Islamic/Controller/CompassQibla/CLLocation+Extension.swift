377bdc960a3c4d30407ba085dceee65be94dad1d
JokeCoder <nshaxzod98@gmail.com>
2020-11-11 21:47:19 +0500

    Qibla compass ready

---
+++ Islamic/Controller/CompassQibla/CLLocation+Extension.swift
@@ -0,0 +1,40 @@
+
+import Foundation
+import CoreLocation
+import UIKit
+
+public extension CLLocation {
+  func bearingToLocationRadian(_ destinationLocation: CLLocation) -> CGFloat {
+    
+    let lat1 = self.coordinate.latitude.degreesToRadians
+    let lon1 = self.coordinate.longitude.degreesToRadians
+    
+    let latOfKabah = 21.4225
+    let lngOfKabah = 39.8262
+
+    let lat2 = latOfKabah.degreesToRadians
+    let lon2 = lngOfKabah.degreesToRadians
+    
+    let dLon = lon2 - lon1
+    
+    let y = sin(dLon) * cos(lat2)
+    let x = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(dLon)
+    let radiansBearing = atan2(y, x)
+    
+    return CGFloat(radiansBearing)
+  }
+  
+  func bearingToLocationDegrees(destinationLocation: CLLocation) -> CGFloat {
+    return bearingToLocationRadian(destinationLocation).radiansToDegrees
+  }
+}
+
+extension CGFloat {
+  var degreesToRadians: CGFloat { return self * .pi / 180 }
+  var radiansToDegrees: CGFloat { return self * 180 / .pi }
+}
+
+private extension Double {
+  var degreesToRadians: Double { return Double(CGFloat(self).degreesToRadians) }
+  var radiansToDegrees: Double { return Double(CGFloat(self).radiansToDegrees) }
+}
---
+++ Islamic/Controller/CompassQibla/CompassViewController.swift
@@ -0,0 +1,78 @@
+
+import UIKit
+import CoreLocation
+
+class CompassViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func back(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    let locationDelegate = LocationDelegate()
+    var latestLocation: CLLocation? = nil
+    var yourLocationBearing: CGFloat { return latestLocation?.bearingToLocationRadian(self.yourLocation) ?? 0 }
+    var yourLocation: CLLocation {
+        get { return UserDefaults.standard.currentLocation }
+        set { UserDefaults.standard.currentLocation = newValue }
+    }
+    
+    let locationManager: CLLocationManager = {
+        $0.requestWhenInUseAuthorization()
+        $0.desiredAccuracy = kCLLocationAccuracyBest
+        $0.startUpdatingLocation()
+        $0.startUpdatingHeading()
+        return $0
+    }(CLLocationManager())
+    
+    private func orientationAdjustment() -> CGFloat {
+        let isFaceDown: Bool = {
+            switch UIDevice.current.orientation {
+            case .faceDown: return true
+            default: return false
+            }
+        }()
+        
+        let adjAngle: CGFloat = {
+            switch UIApplication.shared.statusBarOrientation {
+            case .landscapeLeft:  return 90
+            case .landscapeRight: return -90
+            case .portrait, .unknown: return 0
+            case .portraitUpsideDown: return isFaceDown ? 180 : -180
+            }
+        }()
+        return adjAngle
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        locationManager.delegate = locationDelegate
+        
+        locationDelegate.locationCallback = { location in
+            self.latestLocation = location
+        }
+        
+        locationDelegate.headingCallback = { newHeading in
+            
+            func computeNewAngle(with newAngle: CGFloat) -> CGFloat {
+                let heading: CGFloat = {
+                    let originalHeading = self.yourLocationBearing - newAngle.degreesToRadians
+                    switch UIDevice.current.orientation {
+                    case .faceDown: return -originalHeading
+                    default: return originalHeading
+                    }
+                }()
+                
+                return CGFloat(self.orientationAdjustment().degreesToRadians + heading)
+            }
+            
+            UIView.animate(withDuration: 0.5) {
+                let angle = computeNewAngle(with: CGFloat(newHeading))
+                self.imageView.transform = CGAffineTransform(rotationAngle: angle)
+            }
+        }
+        
+    }
+    
+}
+
+
